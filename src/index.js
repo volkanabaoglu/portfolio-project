f4516cfbb148ab472cd149162a465d40809f79bc
Volkan Abaoglu <abaoglu.2002@gmail.com>
2024-01-22 14:52:44 +0300

    Added all pages component and configured react-routes for pages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import NoPage from "./pages/NoPage";
+import Contact from "./pages/Contact";
+import Skills from "./pages/Skills";
+import AboutMe from "./pages/AboutMe";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="Contact" element={<Contact />} />
+          <Route path="aboutme" element={<AboutMe />} />
+          <Route path="skills" element={<Skills />} />
+          <Route path="*" element={<NoPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
