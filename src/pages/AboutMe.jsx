a65e3ba49f9fe19f267420c4e64be534459d7d40
Volkan Abaoglu <abaoglu.2002@gmail.com>
2024-01-23 17:01:32 +0300

    Updated AboutMe

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -15,25 +15,34 @@ const AboutMe = () => {
   return (
     <>
       <NavbarComponent />
-      <div className="aboutme-container">
-        <br />
-        <br />
-        <br />
-        <br />
+      <div className="aboutme-container">        
 
         <div className="aboutme-p">
           <p>
-            Hello, I'm Volkan, a computer engineering student, and I'm
-            interested in web development, automation systems, and database
-            management. I aim to define myself as a full-stack developer using
-            React and Java technologies, and I'm working towards this goal.
+            Hello, I'm Volkan, a senior computer engineering student at Süleyman
+            Demirel University. My passion lies in web development, automation
+            systems, and database management. Currently, I am actively engaged
+            in creating a dynamic e-commerce platform using MERN (MongoDB,
+            Express.js, React, Node.js) technologies. This project serves as a
+            practical avenue for honing my skills in technologies such as
+            Node.js and Redux.
           </p>
           <br />
           <p>
-            These days, I've been working on developing clone applications and
-            trying to improve myself by sharing my code on GitHub. Additionally,
-            I'm actively attending an English course to improve my language
-            skills.
+            In addition to my academic pursuits, I am dedicated to becoming a
+            proficient full-stack developer, specializing in React and Java
+            technologies. To further enhance my expertise, I regularly
+            contribute to GitHub by sharing my code and collaborating on clone
+            applications. As a continual learner, I am actively enrolled in an
+            English course, striving to improve my language skills for effective
+            communication in the tech community.
+          </p>
+          <p>
+            I am enthusiastic about the prospect of combining my academic
+            knowledge with real-world application, and I am always eager to
+            explore new technologies and methodologies. Feel free to explore my
+            GitHub repository to witness my ongoing journey in the world of
+            programming.
           </p>
         </div>
         <Button
